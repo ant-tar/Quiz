e473c7b2769a6ce9700753b7c10a745601d4b763
Anton Tarasov <himurovich@gmail.com>
2025-11-21 16:30:26 +0400

    Update update.class.php
    zero value isn't possible while I need this one as default. there is temp fix, THINK TWICE! later

--- core/components/quiz/processors/mgr/field_value/update.class.php
+++ core/components/quiz/processors/mgr/field_value/update.class.php
@@ -12,15 +12,18 @@ class QuizFieldValueUpdateProcessor extends modObjectUpdateProcessor
      */
     public function beforeSet()
     {
-        $id = (int) $this->properties['id'];
-        if (empty($id)) {
+        $id = $this->properties['id'];
+
+        if ($id === '' || $id === null) {
             return $this->modx->lexicon($this->objectType . '_err_ns');
         }
+        
+        $id = (int)$id;
 
         $field_id = $this->properties['field_id'];
         foreach (['label', 'value'] as $name) {
             $value = $this->properties[$name];
-            if (empty($value)) {
+            if ($value === '' || $value === null) {
                 $this->modx->error->addField($name, $this->modx->lexicon($this->objectType . '_err_field'));
             } elseif ($this->modx->getCount($this->classKey, [
                 $name => $value,
